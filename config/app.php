df58947a4517f0a9f4cd8fe49ad8b3e6bfdf1a5d
NipunLiyanage69 <nipun.rightmo@gmail.com>
2023-11-12 15:20:19 +0530

    DebugBar Config

--- config/app.php
+++ config/app.php
@@ -185,6 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
