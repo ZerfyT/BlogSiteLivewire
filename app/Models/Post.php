a5bf010b9950164f5d0fdf31718b846dd83a216e
NipunLiyanage69 <nipun.rightmo@gmail.com>
2023-11-12 10:38:46 +0530

    Add Blog Post List Page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
 
     public function scopePublished($query)
     {
@@ -18,5 +23,23 @@ class Post extends Model
     public function scopeFeatured($query)
     {
         $query->where('featured', true);
+    }
+
+    public function auther()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function getReadingTime()
+    {
+        $words = str_word_count($this->body);
+        $minutes =  round($words / 200);
+        return $minutes . ' min read';
+    }
+
+    public function getExcerpt()
+    {
+        // return substr($this->body, 0, 200) . '...';
+        return Str::limit($this->body, 200);
     }
 }
