0ad5a50806b7b99e95f56c7d921eeb9d99721eba
NipunLiyanage69 <nipun.rightmo@gmail.com>
2023-11-28 05:07:38 +0530

    Added Search

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -14,18 +15,29 @@ class PostList extends Component
 
     #[Url()]
     public $sort = 'desc';
+    #[Url()]
+    public $search = '';
 
     #[Computed()]
     public function posts()
     {
-        return Post::published()->orderBy('published_at', $this->sort)->paginate(5);
+        return Post::published()
+            ->orderBy('published_at', $this->sort)
+            ->where('title', 'like', '%'.$this->search.'%')
+            ->paginate(5);
     }
 
     public function setSort($sort)
     {
         $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
     }
-    
+
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
+    }
+
     public function render()
     {
         return view('livewire.post-list');
--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,5 +1,8 @@
 <div class=" px-3 lg:px-7 py-6">
     <div class="flex justify-between items-center border-b border-gray-100">
+        <div class="text-gray-600">
+            @if($search) Searching {{$search}} @endif
+        </div>
         <div class="flex items-center space-x-4 font-light ">
             <button wire:click="setSort('desc')" class="py-4 {{ $sort === 'desc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500'  }}">Latest</button>
             <button wire:click="setSort('asc')" class="py-4 {{ $sort === 'asc' ? 'text-gray-900 border-b border-gray-700' : 'text-gray-500'  }}">Oldest</button>
