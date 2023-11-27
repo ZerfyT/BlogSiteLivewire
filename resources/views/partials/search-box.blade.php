a47eeeefe188db8770ebc69b8cb4a1da7c96ff16
NipunLiyanage69 <nipun.rightmo@gmail.com>
2023-11-28 05:22:16 +0530

    Alpinejs SearchBox

--- resources/views/partials/search-box.blade.php
+++ resources/views/partials/search-box.blade.php
@@ -1,4 +1,4 @@
-<div id="search-box">
+<div x-data="{query: '{{ request('search', '') }}'}" id="search-box">
     <div>
         <h3 class="text-lg font-semibold text-gray-900 mb-3">Search</h3>
         <div class="w-52 flex rounded-2xl bg-gray-100 py-2 px-3 mb-3 items-center">
@@ -9,9 +9,10 @@
                 </svg>
             </span>
             <input
-                wire:model.live.debounce.400ms="search"
+                x-model="query"
                 class="w-40 ml-1 bg-transparent focus:outline-none focus:border-none focus:ring-0 outline-none border-none text-xs text-gray-800 placeholder:text-gray-400"
                 type="text" placeholder="Search">
         </div>
+        <x-button x-on:click="$dispatch('search', {search: query})">Search</x-button>
     </div>
 </div>
